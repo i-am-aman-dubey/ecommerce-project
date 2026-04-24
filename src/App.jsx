e8905fb94e41349e6d54144022581fd466143a7b
i-am-aman-dubey <iamaman5127@gmail.com>
2026-04-24 13:28:47 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect, useState } from "react";
+import Banner from "./components/Banner/Banner";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+
+function App() {
+  const [orderPopup, setOrderPopup] = useState(false);
+
+  const handleOrderpopup = () => {
+    setOrderPopup(!orderPopup)
+  };
+
+  useEffect(()=>{
+    AOS.init({
+      offset:100,
+      duration:800,
+      easing:"ease-in-sine",
+      delay:100
+    })
+    AOS.refresh();
+  }, []);
+
+  return (
+    <>
+      <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+        <Navbar handleOrderpopup={handleOrderpopup}/>
+        <Hero handleOrderpopup={handleOrderpopup}/>
+        <Banner/>
+      </div>
+    </>
+  )
+}
+
+export default App
